9600bb4f5168a0b644d9eae539fd533b7e58aaa4
Bengi Genç <genc.bengi@gmail.com>
2022-12-02 13:12:13 +0300

    Add Greetign component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Greeting from './Greeting';
 
 function App() {
   return (
---
+++ src/Greeting.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Greeting() {
+  return <h1>Greet me!</h1>;
+}
